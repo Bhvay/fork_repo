e7ac287461b57feeedd03473c27f9211d28608b8
bhvay <sreebhavya99033@gmail.com>
2021-09-15 15:33:49 +0530

    christmas

--- culture.py
+++ culture.py
@@ -6,3 +6,5 @@ if festival in hindu_festival:
     print("it is hindu festival")
 if festival in muslim_festival:
     print("it is a  muslim festival")
+if festival in christian_festival:
+    print("it is a christian festival")
