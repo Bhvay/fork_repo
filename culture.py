75aa688db24629d50a811db50ad092d59b6bbd0b
bhvay <sreebhavya99033@gmail.com>
2021-09-15 15:15:14 +0530

    ugadi

---
+++ culture.py
@@ -0,0 +1,6 @@
+hindu_festival=["ugadi","diwali"]
+muslim_festival=["ramzan","bakrid"]
+christian_festival=["christmas","good friday"]
+festival=input("enter any festival")
+for festival in hindu_festival:
+    print("it is hindu festival")
